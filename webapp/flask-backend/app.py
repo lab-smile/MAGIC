1deec13f42681e974ec290729a7500cebfb8447a
Yu Feng <fengyu225@gmail.com>
2023-05-29 20:37:32 -0700

    allow multiple uploaded images

--- webapp/flask-backend/app.py
+++ webapp/flask-backend/app.py
@@ -41,65 +41,65 @@ def cleanup(dir_hash):
 @app.route('/api/upload', methods=['POST'])
 @cross_origin()
 def upload_image():
-    # Get the image
-    image_data_str = request.form['image'].split(",")
-    if len(image_data_str) < 2:
-        return jsonify({"message": "Invalid image"}), 400
-
-    # Open the image with PIL
-    img_data = image_data_str[1].encode("utf-8")
-    img_binary_data = io.BytesIO(base64.decodebytes(img_data))
-    image = Image.open(img_binary_data)
-
-    # Get current timestamp and a random number
-    current_time = str(time.time())
-    random_num = str(random.randint(1, 1e6))
-
-    # Combine the timestamp and random number, and generate a hash
-    hash_input = (current_time + random_num).encode('utf-8')
-    dir_hash = hashlib.md5(hash_input).hexdigest()
-
-    logging.info(f"Directory hash: {dir_hash}")
-
-    upload_dir = f"/var/tmp/uploaded_{dir_hash}"
-    os.makedirs(f"{upload_dir}/test", exist_ok=True)
-
-    # Save the image to the unique directory
-    filename = secure_filename('uploaded_image.bmp')
-    image.save(os.path.join(upload_dir, 'test', filename))
-
-    try:
-        # Run the first shell command
-        subprocess.run(
-            ['/home/ubuntu/.conda/envs/magic_env/bin/python', '/home/ubuntu/MAGIC/src/gpu/pytorch_pix2pix_test.py',
-             '--dataset', upload_dir, '--model_path', '/home/ubuntu/MAGIC/src/MAGIC_Generator_FINAL.pkl'], check=True)
-
-        # Run the second shell command with a specified current working directory
-        subprocess.run(['./run_generate_series.sh', '/usr/local/MATLAB/MATLAB_Runtime/R2023a/', f"{upload_dir}/test",
-                        f"/var/tmp/uploaded_{dir_hash}_results/test_results",
-                        f"/var/tmp/uploaded_{dir_hash}_generate_series_results",
-                        '/home/ubuntu/MAGIC/src/eval/Rapid_Colormap.mat'], check=True,
-                       cwd='/home/ubuntu/compile_generate_series')
-    except subprocess.CalledProcessError:
-        # If the subprocess returned an error, return a HTTP 500 response
-        return jsonify({"message": "Internal Server Error"}), 500
-
-    # Schedule cleanup
-    cleanup_thread = threading.Thread(target=cleanup, args=(dir_hash,))
-    cleanup_thread.start()
-
-    # Image path
-    img_path = f"/var/tmp/uploaded_{dir_hash}_generate_series_results/real/uploaded_image_Real.png"
-
-    # Open the image file in binary mode, read it, and base64 encode its contents
-    with open(img_path, 'rb') as f:
-        img_data = f.read()
-    img_str = base64.b64encode(img_data).decode()
-
-    images = [img_str]
-
     base64_images = []
-    for img_str in images:
+    for key in request.form.keys():
+        # Get the image
+        image_data_str = request.form[key].split(",")
+        if len(image_data_str) < 2:
+            return jsonify({"message": "Invalid image"}), 400
+
+        # Open the image with PIL
+        img_data = image_data_str[1].encode("utf-8")
+        img_binary_data = io.BytesIO(base64.decodebytes(img_data))
+        image = Image.open(img_binary_data)
+
+        # Get current timestamp and a random number
+        current_time = str(time.time())
+        random_num = str(random.randint(1, 1e6))
+
+        # Combine the timestamp and random number, and generate a hash
+        hash_input = (current_time + random_num).encode('utf-8')
+        dir_hash = hashlib.md5(hash_input).hexdigest()
+
+        logging.info(f"Directory hash: {dir_hash}")
+
+        upload_dir = f"/var/tmp/uploaded_{dir_hash}"
+        os.makedirs(f"{upload_dir}/test", exist_ok=True)
+
+        # Save the image to the unique directory
+        filename = secure_filename('uploaded_image.bmp')
+        image.save(os.path.join(upload_dir, 'test', filename))
+
+        try:
+            # Run the first shell command
+            subprocess.run(
+                ['/home/ubuntu/.conda/envs/magic_env/bin/python', '/home/ubuntu/MAGIC/src/gpu/pytorch_pix2pix_test.py',
+                 '--dataset', upload_dir, '--model_path', '/home/ubuntu/MAGIC/src/MAGIC_Generator_FINAL.pkl'],
+                check=True)
+
+            # Run the second shell command with a specified current working directory
+            subprocess.run(
+                ['./run_generate_series.sh', '/usr/local/MATLAB/MATLAB_Runtime/R2023a/', f"{upload_dir}/test",
+                 f"/var/tmp/uploaded_{dir_hash}_results/test_results",
+                 f"/var/tmp/uploaded_{dir_hash}_generate_series_results",
+                 '/home/ubuntu/MAGIC/src/eval/Rapid_Colormap.mat'], check=True,
+                cwd='/home/ubuntu/compile_generate_series')
+        except subprocess.CalledProcessError:
+            # If the subprocess returned an error, return a HTTP 500 response
+            return jsonify({"message": "Internal Server Error"}), 500
+
+        # Schedule cleanup
+        cleanup_thread = threading.Thread(target=cleanup, args=(dir_hash,))
+        cleanup_thread.start()
+
+        # Image path
+        img_path = f"/var/tmp/uploaded_{dir_hash}_generate_series_results/real/uploaded_image_Real.png"
+
+        # Open the image file in binary mode, read it, and base64 encode its contents
+        with open(img_path, 'rb') as f:
+            img_data = f.read()
+        img_str = base64.b64encode(img_data).decode()
+
         base64_images.append(f"data:image/png;base64,{img_str}")
 
     return jsonify({"images": base64_images})
--- webapp/src/App.js
+++ webapp/src/App.js
@@ -6,18 +6,24 @@ import './App.css';
 import logo from './static/smile-lab-logo.png';
 
 function App() {
-  const [image, setImage] = useState(null);
+  const [images, setImages] = useState(null);
   const [responseImages, setResponseImages] = useState([]);
   const [zoomIndex, setZoomIndex] = useState(-1);
   const [uploadedImageZoomed, setUploadedImageZoomed] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleImageChange = (e) => {
-    const file = e.target.files[0];
-    const reader = new FileReader();
-    reader.readAsDataURL(file);
-    reader.onload = () => {
-      setImage(reader.result);
-    };
+    const files = Array.from(e.target.files);
+    const readers = files.map(file => {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      return new Promise((resolve, reject) => {
+        reader.onload = () => resolve(reader.result);
+        reader.onerror = reject;
+      });
+    });
+
+    Promise.all(readers).then(setImages);
   };
 
   const handleImageClick = useCallback((index) => {
@@ -32,16 +38,26 @@ function App() {
     e.preventDefault();
 
     const formData = new FormData();
-    formData.append('image', image);
+    images.forEach((image, i) => {
+      formData.append(`image${i}`, image);
+    });
+
+    const baseUrl = process.env.REACT_APP_API_BASE_URL;
+
+    setIsLoading(true); // set loading state to true
 
     try {
-      const response = await axios.post('http://localhost:5001/api/upload', formData);
+      const response = await axios.post(`${baseUrl}/api/upload`, formData);
 
       setResponseImages(response.data.images);
     } catch (error) {
       console.error('Error while uploading image:', error);
+    } finally {
+      setIsLoading(false); // set loading state to false, whether the request succeeded or failed
     }
   };
+
+  // Other code...
 
   return (
       <div className="App">
@@ -52,42 +68,49 @@ function App() {
           </div>
         </div>
         <div className="content">
-          <div className="left-side">
-            <form onSubmit={handleSubmit}>
-              <input type="file" onChange={handleImageChange}/>
-              <button type="submit">Upload Image</button>
-            </form>
-            {image && (
-                <div
-                    className="uploaded-image-container"
-                    onClick={() => setUploadedImageZoomed(!uploadedImageZoomed)}
-                >
-                  <ControlledZoom
-                      isZoomed={uploadedImageZoomed}
-                      onZoomChange={(isZoomed) => !isZoomed && setUploadedImageZoomed(false)}
-                      zoomMargin={10}
+          {isLoading ? (
+              <div className="loader"/> // show a loading spinner while waiting for the response
+          ) : (
+              <form onSubmit={handleSubmit}>
+                <input type="file" multiple onChange={handleImageChange}/>
+                <button type="submit">Upload Image</button>
+              </form>
+          )}
+          {images && images.map((image, i) => (
+              <div className="row" key={`row${i}`}>
+                <div className="column">
+                  <div
+                      className="uploaded-image-container"
+                      onClick={() => setUploadedImageZoomed(i !== uploadedImageZoomed ? i : -1)}
                   >
-                    <img src={image} alt="Uploaded" className="uploaded-image"/>
-                  </ControlledZoom>
+                    <ControlledZoom
+                        isZoomed={uploadedImageZoomed === i}
+                        onZoomChange={(isZoomed) => !isZoomed && setUploadedImageZoomed(-1)}
+                        zoomMargin={10}
+                    >
+                      <img src={image} alt={`Uploaded ${i + 1}`} className="uploaded-image"/>
+                    </ControlledZoom>
+                  </div>
                 </div>
-            )}
-          </div>
-          <div className="right-side">
-            {responseImages.map((src, index) => (
-                <div key={index} onClick={() => handleImageClick(index)}>
-                  <ControlledZoom
-                      isZoomed={zoomIndex === index}
-                      onZoomChange={(isZoomed) => !isZoomed && setZoomIndex(-1)}
-                      zoomMargin={10}
-                  >
-                    <img src={src} alt={`Response ${index + 1}`} className="response-image"/>
-                  </ControlledZoom>
-                </div>
-            ))}
-          </div>
+                {responseImages[i] && (
+                    <div className="column">
+                      <div className="response-image-container" onClick={() => handleImageClick(i)}>
+                        <ControlledZoom
+                            isZoomed={zoomIndex === i}
+                            onZoomChange={(isZoomed) => !isZoomed && setZoomIndex(-1)}
+                            zoomMargin={10}
+                        >
+                          <img src={responseImages[i]} alt={`Response ${i + 1}`} className="response-image"/>
+                        </ControlledZoom>
+                      </div>
+                    </div>
+                )}
+              </div>
+          ))}
         </div>
       </div>
   );
 }
 
 export default App;
+
